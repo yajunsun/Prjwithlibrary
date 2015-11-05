7d535ccb9c6c57b7f45bcd4dc2ced51bba009409
yajunsun <407250568@qq.com>
2015-11-05 11:09:50 +0800

    add mimidrawer

--- app/src/main/java/sun/com/prjwithlibrary/MateriaDrawerActivity.java
+++ app/src/main/java/sun/com/prjwithlibrary/MateriaDrawerActivity.java
@@ -1,13 +1,20 @@
 package sun.com.prjwithlibrary;
 
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
+import com.mikepenz.aboutlibraries.util.UIUtils;
+import com.mikepenz.crossfader.Crossfader;
 import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.iconics.IconicsDrawable;
 import com.mikepenz.iconics.view.IconicsImageView;
@@ -15,12 +22,18 @@ import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.MiniDrawer;
+import com.mikepenz.materialdrawer.interfaces.OnCheckedChangeListener;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+
+import utils.CrossfadeWrapper;
+import utils.SystemUtils;
 
 /**
  * Created by Administrator on 2015/10/29.
@@ -28,9 +41,9 @@ import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 public class MateriaDrawerActivity extends AppCompatActivity {
     AccountHeader accountHeader;
     Drawer result;
-    //MiniDrawer miniDrawer;
+    MiniDrawer miniDrawer;
     com.mikepenz.iconics.view.IconicsImageView imageView;
-    //Crossfader crossfader;
+    Crossfader crossfader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +54,7 @@ public class MateriaDrawerActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         final IProfile profile = new ProfileDrawerItem().withName("Yajun Sun").withIcon(getResources().getDrawable(R.drawable.yajunsun))
                 .withEmail("407250568@qq.com");
-        accountHeader = new AccountHeaderBuilder().withActivity(this).
+        accountHeader = new AccountHeaderBuilder().withActivity(this).withTranslucentStatusBar(false).
                 withHeaderBackground(getResources().getDrawable(R.drawable.header))
                 .addProfiles(profile).withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
                     @Override
@@ -54,7 +67,7 @@ public class MateriaDrawerActivity extends AppCompatActivity {
                 .build();
         DrawerBuilder builder = new DrawerBuilder()
                 .withActivity(this).withAccountHeader(accountHeader)
-                .withHasStableIds(true).withToolbar(toolbar)
+                .withHasStableIds(true).withToolbar(toolbar).withTranslucentStatusBar(false)
                 .addDrawerItems(new PrimaryDrawerItem().withName("Red").withDescription("It will be paint to red").withIdentifier(R.color.md_red_700)
                         .withIcon(new IconicsDrawable(this, GoogleMaterial.Icon.gmd_adjust).sizeDp(24).colorRes(R.color.md_red_700)))
                 .addDrawerItems(new SecondaryDrawerItem().withName("Red100").withIdentifier(R.color.md_red_100)
@@ -97,37 +110,84 @@ public class MateriaDrawerActivity extends AppCompatActivity {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                         setImageviewSource(drawerItem.getIdentifier());
-                        return false;
-                        //return miniDrawer.onItemClick(drawerItem);
+                        //return false;
+                        return miniDrawer.onItemClick(drawerItem);
                     }
                 }).withSavedInstance(savedInstanceState);
 
-        result=builder.build();
+        result = builder.buildView();
 
-//        miniDrawer = new MiniDrawer().withDrawer(result).withAccountHeader(accountHeader);
-//        int firstWidth = (int) UIUtils.convertDpToPixel(70, this);
-//        int secondWidth = (int) UIUtils.convertDpToPixel(300, this);
-//        crossfader = new Crossfader()
-//                // .withContent(findViewById(R.id.txtcontent))
-//                .withFirst(result.getSlider(), firstWidth)
-//                .withSecond(miniDrawer.build(this), secondWidth)
-//                .withSavedInstance(savedInstanceState)
-//                .build();
-//        miniDrawer.withCrossFader(new CrossfadeWrapper(crossfader));
-//        crossfader.getCrossFadeSlidingPaneLayout().setShadowResourceLeft(R.drawable.material_drawer_shadow_left);
+        miniDrawer = new MiniDrawer().withDrawer(result).withAccountHeader(accountHeader);
+        int firstWidth = (int) UIUtils.convertDpToPixel(300, this);
+        int secondWidth = (int) UIUtils.convertDpToPixel(70, this);
+        crossfader = new Crossfader()
+                .withContent(findViewById(R.id.content))
+                .withFirst(result.getSlider(), firstWidth)
+                .withSecond(miniDrawer.build(this), secondWidth)
+                .withSavedInstance(savedInstanceState)
+                .build();
+        miniDrawer.withCrossFader(new CrossfadeWrapper(crossfader));
+        crossfader.getCrossFadeSlidingPaneLayout().setShadowResourceLeft(R.drawable.material_drawer_shadow_left);
+    }
+
+    void setImageviewSource(int color) {
+        imageView.setImageDrawable(new IconicsDrawable(this, GoogleMaterial.Icon.gmd_directions_boat).sizeDp(240).colorRes(color));
+    }
+
+    private OnCheckedChangeListener onCheckedChangeListener = new OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(IDrawerItem drawerItem, CompoundButton buttonView, boolean isChecked) {
+            if (drawerItem instanceof Nameable) {
+                Log.i("material-drawer", "DrawerItem: " + ((Nameable) drawerItem).getName() + " - toggleChecked: " + isChecked);
+            } else {
+                Log.i("material-drawer", "toggleChecked: " + isChecked);
+            }
+        }
+    };
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //add the values which need to be saved from the drawer to the bundle
+        outState = result.saveInstanceState(outState);
+        //add the values which need to be saved from the accountHeader to the bundle
+        outState = accountHeader.saveInstanceState(outState);
+        //add the values which need to be saved from the crossFader to the bundle
+        outState = crossfader.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        if (SystemUtils.getScreenOrientation() == Configuration.ORIENTATION_LANDSCAPE) {
+            inflater.inflate(R.menu.embedded, menu);
+            menu.findItem(R.id.menu_1).setIcon(new IconicsDrawable(this, GoogleMaterial.Icon.gmd_sort).color(Color.WHITE).actionBar());
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
-
-    void setImageviewSource(int color) {
-        imageView.setImageDrawable(new IconicsDrawable(this, GoogleMaterial.Icon.gmd_directions_boat).sizeDp(320).colorRes(color));
+        //handle the click on the back arrow click
+        switch (item.getItemId()) {
+            case R.id.menu_1:
+                crossfader.crossFade();
+                return true;
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
